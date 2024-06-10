f7206d334c099ac172d6b1c590b95a569369aba1
latifii <latifii68@gmail.com>
2024-06-10 11:36:26 +0330

    test commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default async function NotFound() {
       className="bg-orange-300 text-center text-3xl w-100
     "
     >
-      صحه مورد نظر پیدا نشد
+      صحه مورد نظر پیدا نشد.
     </div>
   );
 }
