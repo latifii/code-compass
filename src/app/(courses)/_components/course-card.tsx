ae0ead881ab6b479d6bfee1aaaa6967a3012c5ab
latifii <latifii68@gmail.com>
2024-07-08 21:08:59 +0330

    add component badge

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/app/_components/badge/badge";
 import { CourseSummary } from "@/types/course-summary.interface";
 import Image from "next/image";
 import Link from "next/link";
@@ -23,17 +24,23 @@ export const CourseCard: React.FC<CourseCardProps> = ({
           height={327}
         />
       </figure>
+      <div className="mt-2 flex gap-2 font-semibold dark:text-info px-3 py-2">
+        <Badge variant="info">{recordStatus}</Badge>
+        <Badge variant="warning">{level}</Badge>
+      </div>
       <div className="card-body">
         <Link href={`/course/${slug}`}>{title}</Link>
         <p>{subTitle}</p>
-        <div>
-          {duration}
-          {basePrice}
+        <div className="flex items-center justify-between">
+          <Badge variant="accent"> {duration}</Badge>
+          {basePrice === 0
+            ? "رایگان"
+            : `${basePrice.toLocaleString("fa-IR")} تومان`}
         </div>
-        <Link href={`/course/${slug}`} className="card-footer justify-center">
-          مشاهده جزئیات دوره
-        </Link>
       </div>
+      <Link href={`/course/${slug}`} className="card-footer justify-center">
+        مشاهده جزئیات دوره
+      </Link>
     </div>
   );
 };
