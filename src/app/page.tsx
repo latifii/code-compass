01093e7af37742f5e1d56616f1510dcada48ad4f
latifii <latifii68@gmail.com>
2024-07-09 12:08:32 +0330

    add svg & feature component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { CourseSummary } from "@/types/course-summary.interface";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
 import { CourseCardList } from "./(courses)/_components/course-card-list";
+import { homeFeatures } from "./data/home-features";
+import Feature from "./_components/feature/feature";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -16,6 +18,13 @@ export default async function Home() {
   return (
     <>
       <HomeHeroSection />
+      <section className="dark:bg-base-75 mt-10">
+        <div className="container py-10 flex flex-col lg:flex-row gap-10 xl:gap-5">
+          {homeFeatures.map((feauture) => (
+            <Feature feature={feauture} key={`key-${feauture.title}`} />
+          ))}
+        </div>
+      </section>
       <section className="container pt-20">
         <div className="text-center xl:text-right">
           <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
