8bec28e34e78d7f58253cfc5e5fe10fef902df2c
latifii <latifii68@gmail.com>
2024-08-04 14:38:06 +0330

    Code refactoring svgIcon components

--- src/app/_components/icons/src/chevron-down.tsx
+++ src/app/_components/icons/src/chevron-down.tsx
@@ -1,10 +1,10 @@
 import BaseIcon from "@/app/_components/icons/base-icon";
-import { SvgIcon } from "@/app/_components/icons/icon.types";
+import { SvgIconProps } from "@/app/_components/icons/icon.types";
 
-export default function SvgIcon(props:SvgIcon) {
+export default function SvgIcon(props: SvgIconProps) {
   return (
     <BaseIcon {...props}>
-      <path d="M21 7.5L12 16.5L3 7.5"/>
+      <path d="M21 7.5L12 16.5L3 7.5" />
     </BaseIcon>
   );
 }
