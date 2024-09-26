27d0531a2a15764e5e5b8185ccc7298d89a33726
latifii <latifii68@gmail.com>
2024-09-26 12:53:28 +0330

    add text-input component

--- src/app/(auth)/signin/components/signin-form.tsx
+++ src/app/(auth)/signin/components/signin-form.tsx
@@ -1,15 +1,45 @@
 "use client";
 
 import { Button } from "@/app/_components/button/button";
+import TextInput from "@/app/_components/form-input/text-input/text-input";
 import Textbox from "@/app/_components/textbox/textbox";
+import { SignIn } from "../types/signin-form";
+import { useForm } from "react-hook-form";
 
 const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignIn>();
+
+  function onSubmit(data: any) {
+    console.log(data);
+  }
   return (
     <>
       <h5 className="text-2xl">ورود | ثبت نام</h5>
       <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
-      <form className="flex flex-col gap-6 mt-16">
-        <Textbox placeholder="شماره موبایل" />
+      <form
+        className="flex flex-col gap-6 mt-16"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <TextInput<SignIn>
+          register={register}
+          name={"mobile"}
+          rules={{
+            required: "شماره موبایل الزامی است",
+            maxLength: {
+              value: 11,
+              message: "شماره موبایل باید 11 رقم باشد",
+            },
+            minLength: {
+              value: 11,
+              message: "شماره موبایل باید 11 رقم باشد",
+            },
+          }}
+          errors={errors}
+        />
         <Button type="submit" variant="primary">
           تایید و دریافت کد
         </Button>
